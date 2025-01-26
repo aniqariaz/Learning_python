45e876852a595bb864028b153ff0f23ba186f24a
Aniqa679 <aniqariaz679@gmail.com>
2025-01-26 21:51:09 +0500

    first code

--- first code.py
+++ first code.py
@@ -38,3 +38,23 @@ a=6
 b=4
 diff=a-b
 print(diff)
+
+# arithmetic operator
+# a=4
+# b=2
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)#remainder
+# print(a**b)#a^b
+
+#relational operator
+a=50
+b=40
+print(a==b)#False
+print(a!=b)#True
+print(a>=b)
+print(a>b)
+print(a<=b)
+print(a<b)
