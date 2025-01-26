4f29abc639c8de4f1a12f167b7fb6ac6347b5225
Aniqa679 <aniqariaz679@gmail.com>
2025-01-26 21:11:10 +0500

    code initialize

---
+++ first code.py
@@ -0,0 +1,40 @@
+# print("hello world" ) #how python give the input
+
+# print("my name is Aniqa","my age is 19")
+
+# print(45)
+# print(67+45)
+
+# # learn about variables
+# name="Aniqa Riaz"
+# age=19
+# price=59.8
+
+# print(name)
+# print(age)
+# print(price)
+
+# print("my name is:",name)
+# print("my age is:",age)
+
+# #value as a variable
+# age2=age
+# print(age2)
+
+# #data type
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# age=None
+# old=False
+
+
+# print(type(old))
+# print(type(age))
+
+# print sum
+a=6
+b=4
+diff=a-b
+print(diff)
