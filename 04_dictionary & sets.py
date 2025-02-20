c9b7e321c39767bf5459e70b55e7cbf7ae8a640f
Aniqa679 <aniqariaz679@gmail.com>
2025-02-20 17:13:18 +0500

    refactor: rename file

--- 04_dictionary & sets.py
+++ 04_dictionary & sets.py
@@ -23,6 +23,39 @@ null_dict ={}
 null_dict["name"]="Aniqa riaz"
 print(null_dict)
 
+#nested dictionary...
+student = {
+    "name": "Aniqa riaz",
+    "subject" :{
+        "physics":97,
+        "math": 80,
+        "com": 91,
+    }
+}
+print((student)["subject"])
+
+# dictionary method..
+
+student = {
+    "name": "Aniqa riaz",
+    "subject" :{
+        "physics":97,
+        "math": 80,
+        "com": 91,
+    }
+}
+print(list(student.keys()))# if we want to type cast in list
+print(len(student))
+
+print(list(student.values()))
+
+print(list(student.items()))
+
+print(student.get("name"))
+
+new_dict ={"city":"karachi"}
+student.update(new_dict)
+print(student)
 
 
 
